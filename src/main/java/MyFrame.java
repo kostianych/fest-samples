77785a746184863b521ce75d383d69ed648f041e
kostianych <kostianych@gmail.com>
2012-02-27 17:34:19 +0300

    Running modal dialog with invokeLater

--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,3 +1,8 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -7,10 +12,25 @@ public class MyFrame extends JFrame {
 
     public static final String LABEL_TEXT = "This is frame label";
 
+	public static final String BUTTON_NAME = "dialogButton";
+
+	public static final String BUTTON_TEXT = "Ok";    
+
     JLabel label;
 
+	private JButton jButton;
+
+	private JDialog dialog;
+
     public MyFrame() {
-	add(getLabel());
+    	add(getLabel());
+    	setName("myFrame");
+    	dialog = new JDialog();
+    	dialog.setSize(300, 200);
+    	dialog.add(getJButton());
+    	dialog.setModal(true);
+    	dialog.setVisible(true);
+    	setVisible(true);
     }
 
     private JLabel getLabel() {
@@ -21,4 +41,25 @@ public class MyFrame extends JFrame {
 	}
 	return label;
     }
+
+    private JButton getJButton() {
+    	if (jButton == null) {
+    		jButton = new JButton();
+    		jButton.setName(BUTTON_NAME);
+    		jButton.setText(BUTTON_TEXT);
+    		jButton.addActionListener(new ActionListener() {
+				
+				public void actionPerformed(ActionEvent e) {					
+					dialog.dispose();
+				}
+			});
+		}
+		return jButton;
+    }
+    
+    public static void main(String[] args) {
+    	MyFrame myFrame = new MyFrame();
+    	myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    	myFrame.setVisible(true);
+	}
 }
--- src/test/java/TestMyDialog.java
+++ src/test/java/TestMyDialog.java
@@ -1,24 +1,69 @@
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.SwingUtilities;
+
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.fest.swing.annotation.RunsInEDT;
+import org.fest.swing.core.BasicRobot;
+import org.fest.swing.core.Robot;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.finder.WindowFinder;
+import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.fixture.FrameFixture;
 import org.fest.swing.fixture.JLabelFixture;
+import java.util.concurrent.TimeUnit;
 
 public class TestMyDialog {    
 
     private FrameFixture myFrame;
+    Robot robot;
     
     @Before
     public void setUp() {
-        myFrame = new FrameFixture(new MyFrame());
-        myFrame.show();
+    	robot = BasicRobot.robotWithNewAwtHierarchy();
+    	createAndShowNew();
+    	//MyFrame frame = createNew();
+    	//robot.showWindow(frame);
+        //myFrame = new FrameFixture(robot, frame);myFrame = WindowFinder.findFrame("myFrame").using(robot);    	
+
+        //myFrame.show();
     }    
+    
+    @RunsInEDT
+    static MyFrame createNew() {
+      return GuiActionRunner.execute(new GuiQuery<MyFrame>() {
+        protected MyFrame executeInEDT() {
+          return new MyFrame();
+        }
+      });
+    }
+    
+    @RunsInEDT
+    static void createAndShowNew() {
+    	SwingUtilities.invokeLater(new Runnable() {
+    		public void run() {
+    			new MyFrame();
+    		}
+    	});	
+    }
+
+
 
     @Test
-    public void testModalDialog() {        
-	JLabelFixture label = myFrame.label(MyFrame.LABEL_NAME);
-	String labelText = label.text();
-	assertEquals(labelText, MyFrame.LABEL_TEXT);    
+    public void testModalDialog() {    	
+    	DialogFixture dialog = WindowFinder.findDialog(JDialog.class).withTimeout(1, TimeUnit.SECONDS).using(robot);
+    	assertNotNull(dialog);
+    	dialog.button(MyFrame.BUTTON_NAME).click();
+    	myFrame = WindowFinder.findFrame("myFrame").using(robot);
+    	JLabelFixture label = myFrame.label(MyFrame.LABEL_NAME);
+    	String labelText = label.text();
+    	assertEquals(labelText, MyFrame.LABEL_TEXT);    
+
     }
 
 }
